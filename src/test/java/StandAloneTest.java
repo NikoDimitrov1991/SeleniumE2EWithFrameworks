54b2381b55c7a62de6ec4182d153b57970811c7d
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-09-05 19:03:04 +0300

    Including the test case before refactoring with PageObject Model Refactoring

--- src/test/java/StandAloneTest.java
+++ src/test/java/StandAloneTest.java
@@ -1,38 +1,65 @@
-import PageObjects.*;
+import PageObjects.LandingPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
-
+import java.util.List;
 
 
 public class StandAloneTest {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args)  {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
 
-        String productName = "ZARA COAT 3";
+
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/client/");
 
-        LandingPage landingPage = new LandingPage(driver);
-        landingPage.goTo();
-        ProductCatalogue productCatalogue = landingPage.loginApplication("ndnikolaydimitrov@gmail.com", "Test123!");
-        productCatalogue.addProductToCart(productName);
-        CartPage cartPage = productCatalogue.goToCartPage();
-        cartPage.verifyProductDisplay(productName);
 
-        Boolean match = cartPage.verifyProductDisplay(productName);
+
+
+        String productName = "ZARA COAT 3";
+        driver.findElement(By.id("userEmail")).sendKeys("ndnikolaydimitrov@gmail.com");
+        driver.findElement(By.id("userPassword")).sendKeys("Test123!");
+        driver.findElement(By.id("login")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+        WebElement prod = products.stream().filter(product -> product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
+        prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+
+        boolean match = cartProducts.stream().anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
         org.testng.Assert.assertTrue(match);
-        CheckoutPage checkoutPage = cartPage.goToCheckout();
-        checkoutPage.selectCountry("Bulgaria");
-        ConfirmationPage confirmationPage = checkoutPage.submitOrder();
+        driver.findElement(By.cssSelector(".totalRow button")).click();
 
-        String confirmMassage = confirmationPage.verifyConfirmationMessage();
-        org.testng.Assert.assertTrue(confirmMassage.equalsIgnoreCase("THANKYOU FOR THE ORDER."),"The confirmation message does not match the expected value.");
-        driver.quit();
+
+        WebElement countryInput = driver.findElement(By.cssSelector("input[placeholder='Select Country']"));
+        countryInput.click();
+        countryInput.sendKeys("Bulgaria");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+
+        driver.findElement(By.xpath("(//span[@class='ng-star-inserted'])[1]")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Place Order']")).click();
+
+        String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        org.testng.Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+        driver.close();
     }
 }
---
+++ src/test/java/TestUsingPageObject.java
@@ -0,0 +1,36 @@
+import PageObjects.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+
+public class TestUsingPageObject {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+
+        String productName = "ZARA COAT 3";
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.goTo();
+        ProductCatalogue productCatalogue = landingPage.loginApplication("ndnikolaydimitrov@gmail.com", "Test123!");
+        productCatalogue.addProductToCart(productName);
+        CartPage cartPage = productCatalogue.goToCartPage();
+        cartPage.verifyProductDisplay(productName);
+
+        Boolean match = cartPage.verifyProductDisplay(productName);
+        org.testng.Assert.assertTrue(match);
+        CheckoutPage checkoutPage = cartPage.goToCheckout();
+        checkoutPage.selectCountry("Bulgaria");
+        ConfirmationPage confirmationPage = checkoutPage.submitOrder();
+
+        String confirmMassage = confirmationPage.verifyConfirmationMessage();
+        org.testng.Assert.assertTrue(confirmMassage.equalsIgnoreCase("THANKYOU FOR THE ORDER."),"The confirmation message does not match the expected value.");
+        driver.quit();
+    }
+}
