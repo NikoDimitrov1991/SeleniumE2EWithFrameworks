0f974803311356319752374274a3529fda3afeac
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-09-10 19:02:26 +0300

    [Add Cucumber framework to the project] Implementing the definitions of @And and @Then

--- src/test/java/stepDefinitions/StepDefinitionImpl.java
+++ src/test/java/stepDefinitions/StepDefinitionImpl.java
@@ -1,13 +1,13 @@
 package stepDefinitions;
 
-import PageObjects.LandingPage;
-import PageObjects.ProductCatalogue;
+import PageObjects.*;
 import TestComponents.BaseTest;
-
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebElement;
-
+import org.testng.Assert;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,6 +16,8 @@ public class StepDefinitionImpl extends BaseTest {
 
     public LandingPage landingPage;
     public ProductCatalogue productCatalogue;
+    public ConfirmationPage confirmationPage;
+
 
     @Given("I landed on Ecommerce Page")
     public void I_landed_on_Ecommerce_Page() throws IOException {
@@ -31,6 +33,24 @@ public class StepDefinitionImpl extends BaseTest {
     public void I_add_product_to_Cart(String productName) throws InterruptedException {
         List<WebElement> products = productCatalogue.getProductList();
         productCatalogue.addProductToCart(productName);
+    }
+
+    @And("^Checkout (.+) and submit the order$")
+    public void Checkout_and_submit_the_order(String productName) {
+        CartPage cartPage = productCatalogue.goToCartPage();
+
+        Boolean match = cartPage.verifyProductDisplay(productName);
+        Assert.assertTrue(match);
+        CheckoutPage checkoutPage = cartPage.goToCheckout();
+        checkoutPage.selectCountry("Bulgaria");
+        confirmationPage = checkoutPage.submitOrder();
+    }
+
+    @Then("{string} message is displayed on ConfirmationPage")
+    public void message_is_displayed_on_ConfirmationPage(String string) {
+        String confirmMessage = confirmationPage.verifyConfirmationMessage();
+        Assert.assertTrue(confirmMessage.equalsIgnoreCase(string));
+        driver.close();
     }
 
 }
