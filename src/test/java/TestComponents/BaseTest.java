bac458046af36397fc9b732fc46b656a53e79241
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-09-10 17:15:00 +0300

    Adding profiles in pom.xml

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -33,7 +33,7 @@ public class BaseTest {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/Resources/GlobalData.properties");
 
         prop.load(fis);
-        String browserName = prop.getProperty("browser");
+        String browserName =  System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
 
         if (browserName.equalsIgnoreCase("chrome")) {
             ChromeOptions options = new ChromeOptions();
